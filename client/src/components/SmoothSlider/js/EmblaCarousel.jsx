22ea654dff0c80771071b8d75a64259b5476bdb3
Amritanshu Rai <amritanshurai13@gmail.com>
2025-10-24 20:08:12 +0530

    feat: Integrate SmoothSlider into HeightPage for mobile height selection and conversion display

--- client/src/components/SmoothSlider/js/EmblaCarousel.jsx
+++ client/src/components/SmoothSlider/js/EmblaCarousel.jsx
@@ -2,16 +2,17 @@ import React from 'react';
 import { IosPickerItem } from './EmblaCarouselIosPickerItem';
 
 const EmblaCarousel = (props) => {
-  const { loop, value, onChange } = props;
+  const { loop, value, onChange, min = 13, max = 100, label = 'years' } = props;
+  const slideCount = max - min + 1;
 
   return (
     <div className='embla'>
       <IosPickerItem
-        slideCount={88}
-        startValue={13}
+        slideCount={slideCount}
+        startValue={min}
         perspective='left'
         loop={loop}
-        label='years'
+        label={label}
         value={value}
         onChange={onChange}
       />
--- client/src/components/SmoothSlider/js/index.jsx
+++ client/src/components/SmoothSlider/js/index.jsx
@@ -3,12 +3,25 @@ import '../css/base.css';
 import '../css/sandbox.css';
 import '../css/embla.css';
 
-const LOOP = true;
+const LOOP = false;
 
-const SmoothSlider = ({ value, onChange }) => (
+const SmoothSlider = ({
+  value,
+  onChange,
+  min = 13,
+  max = 100,
+  label = 'years',
+}) => (
   <main className='sandbox'>
     <section className='sandbox__carousel'>
-      <EmblaCarousel loop={LOOP} value={value} onChange={onChange} />
+      <EmblaCarousel
+        loop={LOOP}
+        value={value}
+        onChange={onChange}
+        min={min}
+        max={max}
+        label={label}
+      />
     </section>
   </main>
 );
